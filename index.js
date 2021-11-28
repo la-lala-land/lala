d9fe61132ee73acd54a494d323e034664a83f802
Caligo <schattenetcaligo@gmail.com>
2021-11-28 20:00:26 +0530

    create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// This is not TypeScript. Passing "true" instead of true would be considered "false" in any functions. Likewise with any other parameter.
+
+const Time = require('./classes/Time')
+
+// You feel like the project is too big to import into your project? 
+// Let's do the magic called "copy/paste".
+// Copy whatever code you want and paste it in yer project
+
+module.exports = {
+    Time
+}
