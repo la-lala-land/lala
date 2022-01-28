e2071ea3fd4152275fa0f86ad815a26e47f22daa
Caligo <schattenetcaligo@gmail.com>
2022-01-28 10:40:01 +0530

    co. to email

--- functions/email.js
+++ functions/email.js
@@ -3,10 +3,10 @@ const namer = require("./namer");
 
 const commonMails = ["gmail.com", "yahoo.com", "mail.com", "email.com", "hotmail.com", "aol.com"]
 
-const endsWith = ["com", "net", "co", "to", "tk", "ml", "ga", "gg", "cc", "de", "in", "jp", "cr", "me", "life", "moe"]
+const endsWith = ["com", "net", "co", "to", "tk", "ml", "ga", "gg", "cc", "de", "in", "jp", "cr", "me", "life", "moe", "us", "uk"]
 
 module.exports = (common=false) => {
   return `${namer(3 + Math.floor(Math.random() * 5)).toLowerCase()}${
     Math.random() < 0.5 ? "." : Math.random() < 0.5 ? "von" : ""
-  }${namer(3 + Math.floor(Math.random() * 5)).toLowerCase()}@${common ? getRandom(commonMails) : `${namer(4 + Math.floor(Math.random() * 3)).toLowerCase()}.${getRandom(endsWith)}`}`;
+  }${namer(3 + Math.floor(Math.random() * 5)).toLowerCase()}@${common ? getRandom(commonMails) : `${namer(4 + Math.floor(Math.random() * 3)).toLowerCase()}.${Math.random() < 0.5 ? "co." : ""}${getRandom(endsWith)}`}`;
 };
