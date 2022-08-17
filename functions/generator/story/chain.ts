74acebb6ba11a2bdf63ade380673a08cb2249d7b
Neko Of The Abyss <psi@innocent.com>
2022-08-17 14:50:58 +0530

    fix imports for chainmail

--- functions/generator/story/chain.ts
+++ functions/generator/story/chain.ts
@@ -1,13 +1,12 @@
 import { capitalize, getRandom } from "../../util/mod.ts";
 import { generateName } from "../name/mod.ts";
-import {
-  death,
-  forward,
-  intro,
-  message,
-  relation,
-  taken,
-} from "../../../db/constants.ts";
+
+import relation from "../../../data/relation.ts";
+import taken from "../../../data/death.ts";
+
+import Synonyms from "../../../data/synonyms.ts";
+
+const { death, forward, intro, message } = Synonyms;
 
 export const generateChainMail = (name?: string): string => {
   return `${getRandom(intro)} ${
